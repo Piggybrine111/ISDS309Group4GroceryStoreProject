335bf95fee849fe100fb63b97f9f9ca8dc67a932
Piggybrine111 <61631913+Piggybrine111@users.noreply.github.com>
2024-12-12 17:39:46 -0800

    Added the 2d array with the inventory and a function that creates a ProjectData folder and the Inventory.txt file inside it if they arent already there when the project boots up.

--- ISDS309Group4GroceryStoreProject/Form1.cs
+++ ISDS309Group4GroceryStoreProject/Form1.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 namespace ISDS309Group4GroceryStoreProject
 {
     public partial class Login : Form
@@ -22,18 +24,106 @@ namespace ISDS309Group4GroceryStoreProject
         {
             if(!Directory.Exists("ProjectData"))
             {
-                string dataFolder = "ProjectData", path = "";
+                InventoryInfo inventoryInfo = new InventoryInfo();
+                string dataFolder = "ProjectData", inv = "", DELIM = ",";
+                int row = 0, invListLength = inventoryInfo.invList.GetLength(0);
+                //int consoleWidth = Console.WindowWidth, spacesToPad = (consoleWidth - line.Length) / 2;
+
+
                 //Make the folder
                 Directory.CreateDirectory(dataFolder);
-                path = Path.Combine(dataFolder, "inventory.txt");
+                //Make file in that folder
+                inv = Path.Combine(dataFolder, "Inventory.txt");
+                //Open the file stream (in Create mode) with access to Write in it
+                FileStream outFile = new FileStream(inv,
+                    FileMode.Create, //replace .Create with .Append to keep old file info
+                    FileAccess.Write);
+                //Open stream writer to write on the file ^
+                StreamWriter writer = new StreamWriter(outFile);
 
-                //Add inventory file to it (opens stream)
-                File.Create(path);
+                writer.WriteLine("This is a list of all the inventory available: ");
 
 
-
-
+                while (row < invListLength)
+                {
+                    //Write each collumn of data for the row onto the file
+                    writer.Write(inventoryInfo.invList[row,0] + DELIM);
+                    writer.Write(inventoryInfo.invList[row,1] + DELIM);
+                    writer.WriteLine(inventoryInfo.invList[row,2]);
+                    //then move onto the next row
+                    ++row;
+                }
+                
+                writer.Close();
+                outFile.Close();
             }
+
+        }
+
+        class InventoryInfo
+        {
+
+            public string[,] invList =
+            {
+                {"Ground Beef", "3.00", "Health"},
+                {"Ribs", "12.50", "NoDiscount"}, 
+                {"Chicken Breast", "3.00", "Health"}, 
+                {"Salmon", "2.50", "Health"}, 
+                {"Bass", "3.25", "NoDiscount"}, 
+                {"Tuna", "2.50", "Health"}, 
+                {"Tri-Tip Stealk", "20.00", "NoDiscount"}, 
+                {"Pork", "2.75", "NoDiscount"}, 
+
+                {"12 Eggs", "3.25", "Health"}, 
+                {"Carrots", "1.50", "Health"}, 
+                {"Brocoli", "1.25", "Health"}, 
+                {"Lettuce", "1.25", "Health"}, 
+                {"Cabbage", "1.50", "Health"}, 
+                {"Kale", "2.00", "Health"}, 
+                {"Green Apple", "0.75", "Health"}, 
+                {"Red Apple", "0.75", "Health"}, 
+                {"Yellow Apple", "0.75", "Health"}, 
+                {"Bell Pepper", "1.25", "NoDiscount"}, 
+                {"Pear", "1.00", "Health"}, 
+                {"Bannana", "1.00", "Health"}, 
+                {"Orange", "1.25", "Health"}, 
+
+                {"Milk Galon", "3.50", "Health"}, 
+                {"Milk Half-Gallon", "2.00", "Health"}, 
+                {"Cheese (Chedar)", "1.50", "NoDiscount"}, 
+                {"Cheese (Mozzarella)", "1.25", "NoDiscount"}, 
+                {"Yogurt", "0.75", "Health"}, 
+                {"Butter", "1.50", "NoDiscount"}, 
+                {"Whipped Cream", "1.00", "NoDiscount"}, 
+
+                {"White Bread", "2.75", "NoDiscount"}, 
+                {"Wheat Bread", "2.50", "Health"}, 
+                {"Flour", "1.50", "NoDiscount"}, 
+                {"12 Cookies", "4.75", "NoDiscount"}, 
+                {"Pumpkin Pie", "6.50", "NoDiscount"}, 
+                {"Apple Pie", "6.50", "NoDiscount"}, 
+                {"Chocolate Cake", "15.00", "NoDiscount"}, 
+                {"Birthday Cake", "16.50", "NoDiscount"}, 
+
+                {"Large Lays Chips", "4.50", "NoDiscount"}, 
+                {"Large Takis", "4.75", "NoDiscount"}, 
+                {"Chip Pack (Variety)", "12.50", "NoDiscount"}, 
+                {"Chocolate Bar", "1.50", "NoDiscount"}, 
+                {"Loli Pop Packets", "5.00", "NoDiscount"}, 
+                {"Ramen", "0.50", "NoDiscount"}, 
+                {"Candy Variety Pack", "15.00", "NoDiscount"}, 
+
+                {"12oz Coke", "3.00", "NoDiscount"}, 
+                {"12oz Sprite", "3.00", "NoDiscount"}, 
+                {"24 Pack Coke", "8.00", "NoDiscount"}, 
+                {"24 Pack Sprite", "8.00", "NoDiscount"}, 
+                {"Soda Variety Pack", "7.50", "NoDiscount"}, 
+                {"12 Pack Beer", "10.00", "NoDiscount"}, 
+                {"Whiskey", "14.00", "NoDiscount"}, 
+                {"Champagne", "16.00", "NoDiscount"}, 
+                {"24 Pack Waters", "5.00", "Health"}, 
+            };
+
 
         }
     }
